c6059b17b7e5a9412a1506d8a172c09cdd0ee5b7
facu1612 <facundon.pesoa@gmail.com>
2018-11-06 22:02:44 -0300

    actividad 1 terminada

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MainActivity.java
@@ -60,11 +60,16 @@ public class MainActivity extends AppCompatActivity implements FragmentManager.O
                                 break;
                             case R.id.optVerMapa:
                                 //TODO HABILITAR
-                                //tag="mapaReclamos";
-                               // fragment =  getSupportFragmentManager().findFragmentByTag(tag);
+                                tag="mapaReclamos";
+
+                                fragment =  getSupportFragmentManager().findFragmentByTag(tag);
                                 //TODO si "fragment" es null entonces crear el fragmento mapa, agregar un bundel con el parametro tipo_mapa
+                                if(fragment==null){
+                                     fragment=new MapaFragment();
+
+                                }
                                 // configurar a la actividad como listener de los eventos del mapa ((MapaFragment) fragment).setListener(this);
-                               // fragmentTransaction = true;
+                               fragmentTransaction = true;
                                 break;
                             case R.id.optHeatMap:
                                 //TODO HABILITAR
